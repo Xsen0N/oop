751281bbe0021d70a5c854c8cc66934b12c89446
Ksenia Zhuk <kseni.zhyk.3@gmail.com>
2022-10-09 16:09:43 +0300

    создание интерфейса с методами и свойствами

---
+++ IProduct.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_4
+{
+     interface IProduct
+    {
+        string Name { get { return "Product"; } }
+        string category { get; set ;}
+        double ProductCost(double cost);
+        void ProductId();
+        void Quality() {
+            Console.WriteLine("Quality is good");
+        }
+    }
+}
