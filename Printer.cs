25cf3fbf9b8d22cc064a6cca3abaab033dae4b0a
Ksenia Zhuk <kseni.zhyk.3@gmail.com>
2022-10-09 16:13:17 +0300

    Дополнительный класс Printer, его метод, содержащий информацию о типе объекта и переопределение ToString

---
+++ Printer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_4
+{
+    public class Printer
+    {
+         public void IAmPrinting(Object someobj) {
+            Console.WriteLine( someobj.GetType());
+            Console.WriteLine(someobj.ToString());
+        }
+    }
+}
