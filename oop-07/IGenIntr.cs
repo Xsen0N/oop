b76770201a0f30c0121db143839c5b9be49d0255
Ksenia <95776427+Xsen0N@users.noreply.github.com>
2022-10-31 20:43:36 +0300

    Создание обобщенного интерфейса

---
+++ oop-07/IGenIntr.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop07
+{
+     interface IGenIntr<T> where  T : new()
+    {
+        void Add(T i);
+        void Remove(T i);
+        void Show();
+    }
+}
