255112c2cfa4b5f2c7ff8570fc357d3766c52830
Ksenia <95776427+Xsen0N@users.noreply.github.com>
2022-11-07 18:53:51 +0300

    добавление 5 методов редактирования строк

---
+++ oop-8/StringAction.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_8
+{
+    public delegate void StrAct(string text);
+
+    public class StringAction
+    {
+        public static void Lower(string str)
+        {
+            Console.WriteLine(str.ToLower());
+        }
+
+        public static void Upper(string str)
+        {
+            Console.WriteLine(str.ToUpper());
+        }
+
+        public static string Reverse(string str)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            return (new string(charArray));
+        }
+
+        public static void Remove(string str)
+        {
+            Console.WriteLine(str.Remove(0, 2));
+        }
+        public static string RemoveEleminstr(string str)
+        {
+            return str.Remove(0, 2);
+        }
+
+        public static void Insert(string str)
+        {
+            Console.WriteLine(str.Insert(str.Length, "@"));
+        }
+    }
+}
