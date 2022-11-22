9fad2dea4c70808578bdf36d2590c8c8f72ad148
Ksenia <kseni.zhyk.3@gmail.com>
2022-11-22 11:45:29 +0300

    implemented all methods

--- oop-11/Program.cs
+++ oop-11/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+                        Student student = new Student("Kate", "Ivanova", 2);
+            Phone phone = new Phone("Александр", 10, 56);
+
+            Console.WriteLine(student);
+
+            Reflector.GetAssemblyName(student.GetType());
+            Reflector.GetAssemblyName(student);
+            Reflector.IsPublicConstructors(student);
+            Reflector.GetAllMethods(student);
+            Reflector.GetFieldsProp(student);
+            Reflector.GetInterface(student);
+            Reflector.GetSpecialMethod(student, "void");
+            Console.WriteLine("\nРабота InvokeMethod:");
+            Reflector.gMethod();
+            Console.WriteLine(Reflector.Create(typeof(Student)));
+            Console.WriteLine(typeof(Student));
+            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - ");
+            Console.WriteLine(phone);
+            Reflector.GetAssemblyName(phone.GetType());
+            Reflector.IsPublicConstructors(phone);
+            Reflector.GetAllMethods(phone);
+            Reflector.GetFieldsProp(phone);
+            Reflector.GetInterface(phone);
+            Reflector.GetSpecialMethod(phone, "void");
         }
     }
 }
